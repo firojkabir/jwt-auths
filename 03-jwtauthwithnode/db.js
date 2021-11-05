d02c9c2bdd48164ed5bf8f0567acf2f1134c4c58
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-11-05 18:53:42 +0100

    03 - Create user validation(input val & existing user val)

---
+++ 03-jwtauthwithnode/db.js
@@ -0,0 +1,10 @@
+const users = [
+    {
+        email: "rimon@gmail.com",
+        password: "asd123"
+    }
+]
+
+module.exports = {
+    users
+}
--- 03-jwtauthwithnode/routes/auth.js
+++ 03-jwtauthwithnode/routes/auth.js
@@ -1,25 +1,42 @@
-const router = require('express').Router()
-const { check, validationResult } = require('express-validator')
+const router = require("express").Router()
+const { check, validationResult } = require("express-validator")
+const { users } = require("../db")
 
-router.post('/signup', [
-    check('email')
+router.post("/signup", [
+    check("email", "Please provide a valid email")
         .isEmail(),
-    check('password')
+    check("password", "Password must be upto 6 characters")
         .isLength({
             min: 6
         })
 ], (req, res) => {
     const { password, email } = req.body
 
+    // Input Validation
     const errors = validationResult(req)
 
-    if(!errors.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(400).json({
-            errors: errors.array
+            errors: errors.array()
         })
     }
 
-    res.send('Auth is working')
+    // Existing User Validation
+    let user = users.find((user) => {
+        return user.email === email
+    })
+
+    if (user) {
+        res.status(400).json({
+            "errors": [
+                {
+                    "msg": "User email already exists!!!"
+                }
+            ]
+        })
+    }
+
+    res.send("Validation passed!!")
 })
 
 module.exports = router
